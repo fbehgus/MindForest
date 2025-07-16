47ca25ef383c1b27aa2f7d13db9501e6432e032e
fbehgus <sinksdo@kakao.com>
2025-07-16 17:06:04 +0900

    Revert "MindForest ver.0"
    This reverts commit 8cde39b0431e8afdaff623ebfccc2c1c4a576558.

--- Assets/Script/inventory/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItemData", menuName = "Inventory/ItemData")]
-public class ItemData : ScriptableObject {
-    public string itemName;              // 아이템 이름
-    public ItemCategory itemType;        // Seed, WaterCan, Shovel
-    public Sprite icon;                  // UI에 띄울 아이콘
-    public GameObject prefab;            // 인스턴스화할 프리팹
-}
